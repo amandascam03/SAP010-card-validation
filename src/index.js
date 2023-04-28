e8f6468b3a402657c0c1f36ce57e9cbbf37c19a6
amandascam03 <amandascam03@gmail.com>
2023-04-28 20:17:38 -0300

    atualização

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import validator from './validator.js';
 
 console.log(validator);
 
+const button = document.getElementById("botao");
+
+button.addEventListener("click", teste);
+
+function teste() {
+    return("Chamou a função teste!");
+}
