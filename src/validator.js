7dc255b6986ec774d653967bc6cc5df605ff2f66
amandascam03 <amandascam03@gmail.com>
2023-05-11 14:58:52 -0300

    dia11

--- src/validator.js
+++ src/validator.js
@@ -2,17 +2,30 @@ const validator = { // atribuindo o validator a uma variavel
 
     isValid: function (numero) { //o meu isValid é um objeto, e minha função uma propriedade
        let ccNumber = numero.split('').map(Number).reverse(); // a variavel ccNumber pega os numeros e coloca eles em uma array, revertendo de trás pra frente
-      //somaPar = 0; // as variáveis são definidas para pegar o valor de numeros na posição impar, par e o valor final quando forem somados
-       //somaImpar = 0;
-       //valorFinal = 0;
+      
        console.log(ccNumber, "está pegando os numeros do cartao e colocando em uma array");
-      let totalNumbers = 0;
-       for (let i = 0; i < ccNumber.length; i++){
-        if (i % 2 !== 0) {
-        let multip = ccNumber[i] * 2;
-        console.log(multip);
-        }
-        }
+       let newArray = [];
+        for (let i = 0; i < ccNumber.length; i++){
+          if (i % 2 !== 0) {
+          const multip = ccNumber[i] * 2;
+          newArray.push(multip);
+          console.log(newArray);
+          if (multip >=10) {
+            const multiplo = multip % 10 + 1;
+            newArray.push(multiplo);
+            console.log(newArray);
+          }
+          else {
+            newArray.push(multip);
+            console.log(newArray);
+          }
+          }
+          else {
+            ccNumber[i];
+            newArray.push(ccNumber[i]);
+            console.log(newArray);
+          }
+      }
   },
     //maskify: function(valor){
 
