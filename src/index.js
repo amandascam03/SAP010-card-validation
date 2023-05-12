c24bd0f51c8784a00161c63c0c31500fd9645520
amandascam03 <amandascam03@gmail.com>
2023-05-12 15:07:00 -0300

    finalizando projeto

--- src/index.js
+++ src/index.js
@@ -8,47 +8,50 @@ em seguida fiz uma função para o preventDefault, porque não quero enviar o va
 botao.addEventListener("click", function(e) {
     e.preventDefault();
 
-    //fiz uma variavel const para pegar o valor do input
-    //const numeroInserido = document.getElementById("numero");
+    let valor = document.getElementById("numero").value; // variavel para pegar o numero inserido no input
+    const resultado = validator.isValid(valor); // variavel para chamar a função e executar com o valor inserido
+    console.log(resultado);
+    const maskified = validator.maskify(valor);
+    console.log(maskified);
 
-    //fiz uma const valor para poder mostrar o valor inserido no input na string retornada no <p>
-    let valor = document.getElementById("numero").value;
-
-    //let resultado = validator.isValid(numero);
-    //let maskify = validator.maskify
-
-    /*fiz um getElementById para mostrar na tela em um paragrafo <p> se a quantidade de dígitos do cartão que o usuário inseriu é
-suficiente ou não, usando if e else para as condições*/
-    if (valor.length == 16) {
-        document.getElementById("alerta").style.border = 'thick outset green';
-        document.getElementById("alerta").style.backgroundColor = 'green';
-        document.getElementById("alerta").innerHTML = `Parabéns! seu cartão de número ${valor} é válido.`;
+    //const maskified = validator.maskify(valor);
+    //console.log(maskified);
+    /*o getElementById pega no HTML o paragrafo <p> de id "alerta" e mostra se a quantidade de dígitos do cartão que o usuário inseriu é
+suficiente ou não, se o campo está vazio e se é true ou false, usando if e else para as condições*/
+    
+    if (valor.length === 0) {
+        document.getElementById("alerta").style.border = 'thick outset red';
+        document.getElementById("alerta").style.backgroundColor = 'red';
+        document.getElementById("alerta").innerHTML = "O campo está vazio. Por favor, insira os dígitos do cartão.";
     }
-    else if (valor.length >= 1 && valor.length <=15) {
+    else if (valor.length >= 1 && valor.length <= 15) {
         document.getElementById("alerta").style.border = 'thick outset red';
         document.getElementById("alerta").style.backgroundColor = 'red';
         document.getElementById("alerta").innerHTML = "Por favor, insira até 16 dígitos.";
     }
-    else if (valor.length == 0) {
-        document.getElementById("alerta").style.border = 'thick outset red';
-        document.getElementById("alerta").style.backgroundColor = 'red';
-        document.getElementById("alerta").innerHTML = "Por favor, insira os dígitos do cartão.";
-    }
-    else if (valor.length >=17) {
+    else if (valor.length >= 17) {
         document.getElementById("alerta").style.border = 'thick outset red';
         document.getElementById("alerta").style.backgroundColor = 'red';
         document.getElementById("alerta").innerHTML = "Você inseriu muitos números.";
+    }
+    else if (resultado === true) {
+        document.getElementById("alerta").style.border = 'thick outset green';
+        document.getElementById("alerta").style.backgroundColor = 'green';
+        document.getElementById("alerta").innerHTML = `Parabéns! seu cartão de número ${maskified} é válido.`;
     }
     else {
         document.getElementById("alerta").style.border = 'thick outset red';
         document.getElementById("alerta").style.backgroundColor = 'red';
         document.getElementById("alerta").innerHTML = "Infelizmente esse cartão é inválido.";
     }
+
 });
 
 //teste
-validator.isValid("1233456");
-const resultado = validator.isValid(numero);
-console.log(resultado);
+//let valor = document.getElementById("numero").value;
+//const resultado = validator.isValid("1233456");
+//const resultado = validator.isValid(valor);
+//console.log(resultado);
+//console.log(validator.maskify(1234567891234567));
 
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,33 +1,29 @@
-const validator = { // atribuindo o validator a uma variavel (ARRAY 1)
+const validator = { //atribuindo o validator a uma variavel
 
-    isValid: function (numero) { //o meu isValid é um objeto, e minha função (numero) uma propriedade
-       let ccNumber = numero.split('').map(Number).reverse(); // a variavel ccNumber pega os numeros e coloca eles em uma array, revertendo de trás pra frente
+    isValid: function(numero) { //o meu isValid é um objeto, e minha função (numero) uma propriedade
+    console.log(numero);
+    let ccNumber = numero.split('').map(Number).reverse(); // a variavel ccNumber pega os numeros e coloca eles em uma array, revertendo de trás pra frente
       
-       console.log(ccNumber, "está pegando os numeros do cartao e colocando em uma array");
+    console.log(ccNumber);
 
-       let newArray = [];
+    let newArray = [];
        
-        for (let i = 0; i < ccNumber.length; i++){ // (ARRAY 2) o for inicializa o i como 0, e acrescenta 1 (repete a ação) enquanto o i for menor que o comprimento da array
-          if (i % 2 !== 0) { //a condição verifica se o numero esta na posição impar
+    for (let i = 0; i < ccNumber.length; i++){ //o for inicializa o i como 0, e acrescenta 1 (repete a ação) enquanto o i for menor que o comprimento da array
+      if (i % 2 !== 0) { //a condição verifica se o numero esta na posição impar
           const multip = ccNumber[i] * 2; // a variavel multip faz a multiplicação se o numero estiver na posição impar
-          //newArray.push(multip);
-          //console.log(newArray);
           if (multip >=10) { //se a multiplicação der um número igual ou acima de dez, a const multiplo vai pegar o resto da divisão do numero por 10 e somar com 1, resultando sempre em um numero de 0 a 9
             const multiplo = multip % 10 + 1;
             newArray.push(multiplo); // a função .push() empurra dentro da array o resultado
-            //console.log(newArray);
           }
           else {
-            newArray.push(multip); //se a multiplicação menor que 10, a função .push() empurra dentro da array apenas o resultado da multiplicação
-            //console.log(newArray); 
-          }
-          }
-          else {
-            ccNumber[i]; // se o numero estiver na posição par, a função .push() empurra dentro da array ele mesmo
-            newArray.push(ccNumber[i]);
-            //console.log(newArray);
+            newArray.push(multip); //se a multiplicação der menor que 10, a função .push() empurra dentro da array apenas o resultado da multiplicação
           }
       }
+        else {
+          ccNumber[i]; // se o numero estiver na posição par, a função .push() empurra dentro da array ele mesmo
+          newArray.push(ccNumber[i]);
+        }
+    }
         console.log(newArray);
 
         let soma = 0;
@@ -35,12 +31,21 @@ const validator = { // atribuindo o validator a uma variavel (ARRAY 1)
           soma += newArray[s];
           console.log(soma);
         }
-          
         return soma % 10 === 0;
-  },
-    //maskify: function(valor){
 
-    //}
+    },
+      maskify: function(numeroMascarado){
+        var string = numeroMascarado.toString();
+        var arr = string.split('');
+        console.log(arr);
+        for (var i = 0; i < arr.length - 4; i++){
+          arr[i] = '#';
+          console.log(arr);
+        }
+        var masked = arr.join('');
+        //console.log(masked);
+        return masked;
+      }
 };
 
 export default validator;
